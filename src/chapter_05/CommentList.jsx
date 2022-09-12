56ec26313ac12038d78b99376e8859f262f9a3f0
matt700395 <86474039+matt700395@users.noreply.github.com>
2022-09-12 19:28:11 +0900

    chapter5-2

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -4,7 +4,8 @@ import Comment from "./Comment";
 function CommentList(props){
     return (
         <div>
-            <Comment />
+            <Comment name={"이인제"} comment={"안녕하세요, 소플입니다."}/>
+            <Comment name={"유재석"} comment={"리액트 재미있습니다!"}/>
         </div>
     )
 }
